15f15fd55c0fe0ad10672d3d33aec697c458cc22
Long Do <long.do@cyberlogitec.com>
2025-07-01 20:22:40 +0700

    Bertopic create

--- data/index_doc.py
+++ data/index_doc.py
@@ -3,14 +3,16 @@ import torch
 import json
 import os
 from tqdm.auto import tqdm
+from typing import List
 from transformers import AutoModel
 
 from langchain_community.docstore.in_memory import InMemoryDocstore
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface.embeddings.huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores.faiss import DistanceStrategy
-
 from langchain_core.documents import Document
+
+from bertopic import BERTopic
 
 batch_encode_size = 256
 
@@ -40,6 +42,8 @@ def create_index(model_name: str="sentence-transformers/all-MiniLM-L6-v2"):
 def load_doc(doc_path:str = "passage_corpus.json"):
     batch_doc = []
     embed_collection = None
+    # Collection for Bertopic
+    passage_collect = []
 
     with open(doc_path, "r") as f:
         ls_pid = []
@@ -51,16 +55,25 @@ def load_doc(doc_path:str = "passage_corpus.json"):
             batch_doc.append(Document(
                 page_content=obj["ref_string"],
             ))
+            passage_collect.append(obj["ref_string"])
 
-    return ls_pid,batch_doc
+    return ls_pid,batch_doc,passage_collect
 
+def create_bertopic_collection(passage_collect:List, model_name:str):
+    topic_model = BERTopic(embedding_model=model_name)
+    topics, probs = topic_model.fit_transform(passage_collect)
+    
+    return topic_model
 def create_vector_store(doc_path:str = "passage_corpus.json",model_name: str="sentence-transformers/all-MiniLM-L6-v2"):
 
-    ls_pid,batch_doc = load_doc(doc_path)
+    save_name = model_name.split("/")[-1]
+    ls_pid,batch_doc,passage_collect = load_doc(doc_path)
     vector_store = create_index(model_name)
     
+    # Create vector store
     tmp_doc = []
     tmp_pid = []
+
     for (pid,text_embedding) in tqdm(zip(ls_pid,batch_doc),total= len(ls_pid)):
         tmp_doc.append(text_embedding)
         tmp_pid.append(pid)
@@ -73,7 +86,12 @@ def create_vector_store(doc_path:str = "passage_corpus.json",model_name: str="se
     vector_store.add_documents(documents=tmp_doc, ids=tmp_pid)
 
     os.makedirs("data/vector_store_doc", exist_ok=True)
-    vector_store.save_local("data/vector_store_doc/faiss_CORAL_MiniLML6_v2")
+    vector_store.save_local(f"data/vector_store_doc/faiss_CORAL_{save_name}")
+
+    # Create Bertopic collection
+    topic_model = create_bertopic_collection(passage_collect=passage_collect,model_name=model_name)
+    os.makedirs("data/bertopic", exist_ok=True)
+    topic_model.save(f"./BerTopic_corpus_{save_name}")
 
 if __name__ == '__main__':
     doc_path = "data/raw/passage_corpus.json"
